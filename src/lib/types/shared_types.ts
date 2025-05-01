df6a7f1aabe435acab94b271eef4e7e9b19acf29
KigoJomo <kigojomo@gmail.com>
2025-05-01 21:56:01 +0300

    Code format update

--- src/lib/types/shared_types.ts
+++ src/lib/types/shared_types.ts
@@ -1,4 +1,4 @@
-export enum AiModel{
+export enum AiModel {
   GEMINI_2_0_FLASH = 'gemini-2.0-flash',
   GEMINI_2_0_FLASH_LITE = 'gemini-2.0-flash-lite',
   GEMINI_2_0_PRO = 'gemini-2.0-pro',
@@ -7,12 +7,12 @@ export enum AiModel{
 }
 
 export const modelDisplayNames: { [key in AiModel]: string } = {
-  [AiModel.GEMINI_2_0_FLASH]: "Gemini 2.0 Flash",
-  [AiModel.GEMINI_2_0_FLASH_LITE]: "Gemini 2.0 Flash Lite",
-  [AiModel.GEMINI_2_0_PRO]: "Gemini 2.0 Pro",
-  [AiModel.GEMINI_2_5_FLASH]: "Gemini 2.5 Flash",
-  [AiModel.GEMINI_2_5_PRO]: "Gemini 2.5 Pro",
-}
+  [AiModel.GEMINI_2_0_FLASH]: 'Gemini 2.0 Flash',
+  [AiModel.GEMINI_2_0_FLASH_LITE]: 'Gemini 2.0 Flash Lite',
+  [AiModel.GEMINI_2_0_PRO]: 'Gemini 2.0 Pro',
+  [AiModel.GEMINI_2_5_FLASH]: 'Gemini 2.5 Flash',
+  [AiModel.GEMINI_2_5_PRO]: 'Gemini 2.5 Pro',
+};
 
 export type Role = 'user' | 'model';
 
@@ -42,16 +42,16 @@ export interface Chat {
 
 export interface ChatContextType {
   messages: Message[];
-  updateMessages: (messages: Message[]) => void
+  updateMessages: (messages: Message[]) => void;
   responding: boolean;
   chatId: string | null;
-  updateChatId: (newId: string) => void
-  chatTitle: string | null
-  updateChatTitle: (newTitle: string) => void
+  updateChatId: (newId: string) => void;
+  chatTitle: string | null;
+  updateChatTitle: (newTitle: string) => void;
   sendMessage: (prompt: string) => Promise<void>;
-  clearChat: () => void,
-  chats: Chat[],
-  refreshChatList: () => Promise<void>,
-  selectedModel: AiModel,
-  updateSelectedModel: (model: AiModel) => void
+  clearChat: () => void;
+  chats: Chat[];
+  refreshChatList: () => Promise<void>;
+  selectedModel: AiModel;
+  updateSelectedModel: (model: AiModel) => void;
 }
